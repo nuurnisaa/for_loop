edd9303b0470428ffc651faa543ce953b95be263
Nuur <103619888+nuurnisaa@users.noreply.github.com>
2023-04-14 12:56:53 +0800

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,13 @@
+friends=["NICE", "NOT NICE", "VERY NICE", "COOL" ]
+
+
+for letter in range(len(friends)):
+    print (friends[letter])
+    
+for index in range(10) :
+    if index==0:
+        print("First")
+    elif index>=4:
+        print("Reach")
+    else:
+        print("Not first")
